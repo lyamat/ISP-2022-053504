223fbb4a42f8931b757df62ba8f5b24831676146
Ilya <ilyamatveev12902@gmail.com>
2022-03-22 05:31:46 +0300

    first release

---
+++ task_2/README.md
@@ -0,0 +1,7 @@
+# Parser
+
+## _Json | Pickle | Toml | Yaml_ Parser
+
+Encoder and decoder. 
+Compatible with default parsers.
+Supports primitives, functions, classes.
---
+++ task_2/universal_parser/serializers/yaml_serializer/yaml_serializer.py
@@ -0,0 +1,18 @@
+import yaml
+from universal_parser.object_converter import refactor_object, restore_object
+
+
+class YamlSerializer:
+    def dumps(self, obj):
+        return yaml.dump(refactor_object(obj))
+
+    def dump(self, obj, fp):
+        with open(fp, "w") as outfile:
+            yaml.dump(refactor_object(obj), outfile)
+
+    def loads(self, obj):
+        return restore_object(yaml.load(obj, Loader=yaml.FullLoader))
+
+    def load(self, fp):
+        with open(fp, "r") as outfile:
+            return restore_object(yaml.load(outfile.read(), Loader=yaml.FullLoader))
